a974e605ad01f2c5777d3e96f7008377b0f8d086
Tetra <me@tetrafox.pw>
2019-10-26 23:37:31 -0400

    fix np

--- assets/js/nowplaying.js
+++ assets/js/nowplaying.js
@@ -1,14 +1,21 @@
-const URI = "http://lastfm-tetrafox-pw.herokuapp.com/";
+const URI = "https://mercury-chungkingosaurus.glitch.me/";
 
 const req = new XMLHttpRequest();
-req.open("GET", URI, false);
-req.send(null);
 
-const res = JSON.parse(req.response);
-const np = `${res.artist.toLowerCase()} - ${res.name.toLowerCase()}`;
-document.getElementById("nowplaying").innerText = np;
-
-
-if (!res.nowPlaying) {
-    document.getElementById("nowplaying").style.display = "none";
+const poll = () => {
+    req.open("GET", URI, true);
+    req.onload = () => {
+        if (req.readyState === 4 && req.status === 200) {
+            const res = JSON.parse(req.responseText);
+            if (res.nowPlaying) {
+                const np = `${res.artist.toLowerCase()} - ${res.name.toLowerCase()}`;
+                document.getElementById("nowplaying").innerText = np;
+                document.getElementById("nowplaying").style.display = "inline";
+            }
+        }
+    };
+    req.send();
 }
+
+poll();
+setInterval(poll, 10000);
