492ea12dffa1e975e3881108e825619dd3f3f247
tetra <me@tetra.cool>
2026-03-27 23:22:44 -0700

    update nowplaying websocket

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,21 +1,58 @@
 import { readable, type Readable } from "svelte/store";
-import { io } from "socket.io-client";
 
-export const song: Readable<Song> = readable({ artist: "", title: "", playing: false }, (set) => {
-  const socket = io("wss://mercury-chungkingosaurus.glitch.me");
+export const song: Readable<Track | null> = readable(null, (set) => {
+  let ws: WebSocket;
+  let reconnectTimer: ReturnType<typeof setTimeout>;
 
-  socket.on("track", (data) => {
-    set({ artist: data.artist, title: data.name, playing: data.nowPlaying });
-  });
+  const connect = () => {
+    ws = new WebSocket("wss://np.mesa.tetra.cool/ws");
+
+    ws.addEventListener("message", (event) => {
+      set(JSON.parse(event.data));
+    });
+
+    ws.addEventListener("close", () => {
+      reconnectTimer = setTimeout(connect, 3000);
+    });
+  };
+
+  connect();
 
   return () => {
-    socket.close();
+    clearTimeout(reconnectTimer);
+    ws?.close();
   };
 });
 
-export const links: Readable<NamedLink[]> = readable([
-  {
-    name: "E-mail",
-    url: "mailto:me@tetra.cool"
-  }
-]);
+export const links: Readable<Record<string, string>> = readable({
+  "Bluesky": "https://bsky.app/profile/tetra.cool",
+  "Soundcloud": "https://soundcloud.com/tetrafft",
+  "Bandcamp": "https://tetrafft.bandcamp.com",
+  "Spotify": "https://open.spotify.com/artist/3BCEPJ0GHgaZzCD5cSTEjv?si=vQ0-SYq4S6m_oiauOsS4Ww",
+  "Apple Music": "https://itunes.apple.com/us/artist/tetra/1470017364",
+  "GitHub": "https://github.com/tetra-fox",
+  "E-mail": "mailto:me@tetra.cool"
+});
+
+interface Track {
+  mbid: string,
+  name: string,
+  url: string,
+  artist: NamedEntity,
+  album: NamedEntity,
+  image: Image,
+  streamable: boolean,
+  nowplaying: boolean,
+}
+
+interface NamedEntity {
+  mbid: string,
+  name: string
+}
+
+interface Image {
+  small: string,
+  medium: string,
+  large: string,
+  extralarge: string
+}
